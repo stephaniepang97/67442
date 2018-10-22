519bce257c70c29b2bf7813638cdaac1324977ff
stephaniepang97 <stephaniepang97@gmail.com>
2018-10-21 23:17:07 -0400

    Basic relationships, scopes for family and user

--- ThoughtfulAPI/app/models/family.rb
+++ ThoughtfulAPI/app/models/family.rb
@@ -1,2 +1,12 @@
 class Family < ApplicationRecord
+
+  # Relationships
+  belongs_to :user, foreign_key: :patient_id
+  has_many :users, foreign_key: :family_id
+
+  # Validations
+  validates_presence_of :family_name
+
+  # Scopes
+  scope :alphabetical, -> { order('family_name')}
 end
--- ThoughtfulAPI/app/models/user.rb
+++ ThoughtfulAPI/app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+
+  # Relationships
+  has_one :family, foreign_key: :patient_id
+  belongs_to :family, foreign_key: :family_id
+
+  # Validations
+  validates_presence_of :first_name, :last_name, :role, :family_id
+
+  # Scopes
+  scope :alphabetical, -> { order('last_name, first_name')}
+
+  # Other methods
+  def name
+    last_name + ", " + first_name
+  end
+
+  def proper_name
+    first_name + " " + last_name
+  end
 end
