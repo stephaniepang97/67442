2eb19e73aa251db5f5c7f7ac7ab0907b95c91c1b
stephaniepang97 <stephaniepang97@gmail.com>
2018-10-22 00:48:48 -0400

    migrate

--- ThoughtfulAPI/db/schema.rb
+++ ThoughtfulAPI/db/schema.rb
@@ -19,14 +19,6 @@ ActiveRecord::Schema.define(version: 20181022032052) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "images", force: :cascade do |t|
-    t.string "attachment"
-    t.string "label"
-    t.integer "uploaded_by"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "patient_sessions", force: :cascade do |t|
     t.integer "patient_id"
     t.datetime "start_time"
@@ -39,18 +31,18 @@ ActiveRecord::Schema.define(version: 20181022032052) do
     t.string "question"
     t.string "answer"
     t.integer "created_by"
-    t.integer "image_id"
+    t.string "attachment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "session_questions", force: :cascade do |t|
-    t.integer "patient_session_id_id"
-    t.integer "question_id_id"
+    t.integer "patient_sessions_id"
+    t.integer "questions_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["patient_session_id_id"], name: "index_session_questions_on_patient_session_id_id"
-    t.index ["question_id_id"], name: "index_session_questions_on_question_id_id"
+    t.index ["patient_sessions_id"], name: "index_session_questions_on_patient_sessions_id"
+    t.index ["questions_id"], name: "index_session_questions_on_questions_id"
   end
 
   create_table "users", force: :cascade do |t|
