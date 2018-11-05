a4270b63e37db64210c74b5ff2433687060d106e
Alec Lam <alechunterlam@gmail.com>
2018-11-05 10:49:19 -0500

    Pushing after jordan meeting

---
+++ Thoughtful/Thoughtful/Controllers/HomeController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeController.swift
+//  Thoughtful
+//
+//  Created by Alec Lam on 11/5/18.
+//  Copyright © 2018 Stephanie Pang. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+		
+	}
+}
+
