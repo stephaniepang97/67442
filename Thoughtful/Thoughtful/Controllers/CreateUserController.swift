fff4c7ab662d57e6ae33769817054832cc65e67a
Alec Lam <alechunterlam@gmail.com>
2018-11-21 01:57:55 -0500

    Able to create users

--- Thoughtful/Thoughtful/Controllers/CreateUserController.swift
+++ Thoughtful/Thoughtful/Controllers/CreateUserController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class CreateUserController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 	
 	@IBOutlet weak var rolePicker: UIPickerView!
-	
+	@IBOutlet weak var firstNameInput: UITextField!
+	@IBOutlet weak var lastNameInput: UITextField!
+	@IBOutlet weak var familyNameInput: UITextField!
+
+	var selectedValue : String = ""
 	var roleData: [String] = [String]()
 	
 	override func viewDidLoad() {
@@ -19,10 +25,11 @@ class CreateUserController: UIViewController, UIPickerViewDelegate, UIPickerView
 		// Do any additional setup after loading the view, typically from a nib.
 		
 		roleData = ["Patient", "Caretaker", "Doctor"]
-		
+		self.selectedValue = roleData[0]
 		// connector
 		self.rolePicker.delegate = self
 		self.rolePicker.dataSource = self
+		
 	}
 	
 	override func didReceiveMemoryWarning() {
@@ -46,19 +53,52 @@ class CreateUserController: UIViewController, UIPickerViewDelegate, UIPickerView
 		return roleData[row]
 	}
 	
-	func createUserRequest() {
-		let parameters: Parameters = [
-			"foo": "bar",
-			"baz": ["a", 1],
-			"qux": [
-				"x": 1,
-				"y": 2,
-				"z": 3
-			]
-		]
+	// Capture the picker view selection
+	func pickerView(_ pickerView: UIPickerView, didSelectRow inComponent: Int, inComponent component: Int) {
+		// This method is triggered whenever the user makes a change to the picker selection.
+		// The parameter named row and component represents what was selected.
+		self.selectedValue = roleData[inComponent]
+	}
+	
+	@IBAction func createUserRequest() {
+		print("hi")
+		var familyName = ""
+		if let temp = familyNameInput.text {
+			familyName = temp
+		}
 		
+		Alamofire.request("https://thoughtfulapi.herokuapp.com/families").responseJSON { response in
+			var familyId = -1
+			let families = response.result.value
+			let swifty = JSON(families as Any).arrayObject
+			if let swiftySafe = swifty {
+				for index in 0..<swiftySafe.count {
+					var family = JSON(swiftySafe[index])
+					let currentFamilyName = family["family_name"].string
+					let currentFamilyId = family["family_id"].int!
+					if  (familyName == currentFamilyName) {
+						familyId = currentFamilyId
+						break
+					}
+				}
+			}
+			let parameters: Parameters = [
+				"first_name": self.firstNameInput.text!,
+				"last_name": self.lastNameInput.text!,
+				"family_id": familyId,
+				"role": self.selectedValue.lowercased()
+			]
+			print(parameters)
+			Alamofire.request("https://thoughtfulapi.herokuapp.com/users", method: .post, parameters: parameters).responseJSON
+				{ response in print(response)
+			}
+
+		}
+		
+		
+
 		// All three of these calls are equivalent
-		Alamofire.request("https://httpbin.org/post", method: .post, parameters: parameters)
+//		Alamofire.request("https://httpbin.org/post", method: .post, parameters: parameters)
 	}
 	
 	
