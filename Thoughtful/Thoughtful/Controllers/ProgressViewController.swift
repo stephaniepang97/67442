5c4b3cee2eac13fa6ed5ec26979bd8c286e695e2
Alec Lam <alechunterlam@gmail.com>
2018-12-04 12:47:22 -0500

    Added Charts Cocoapod

---
+++ Thoughtful/Thoughtful/Controllers/ProgressViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProgressViewController.swift
+//  Thoughtful
+//
+//  Created by Alec Lam on 12/4/18.
+//  Copyright © 2018 Stephanie Pang. All rights reserved.
+//
+
+import Foundation
+import Charts
+
+
+class ProgressViewController {
+	
+}
