05b751c6439a92680820040e23a3cff23f81939e
Alec Lam <alechunterlam@gmail.com>
2018-12-04 14:38:34 -0500

    Basic functional graph implemented

--- Thoughtful/Thoughtful/Controllers/ProgressViewController.swift
+++ Thoughtful/Thoughtful/Controllers/ProgressViewController.swift
@@ -10,6 +10,39 @@ import Foundation
 import Charts
 
 
-class ProgressViewController {
+class ProgressViewController: UIViewController  {
+	@IBOutlet var chartView: BarChartView!
+	
+	var months: [String]!
+	
+	override func viewDidLoad() {
+		
+		chartView.noDataText = "No patient data available"
+	
+		months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+		let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+		
+		setChart(dataPoints: months, values: unitsSold)
+
+	}
+	
+	func setChart(dataPoints: [String], values: [Double]) {
+		var dataEntries: [BarChartDataEntry] = Array()
+		var counter = 0.0
+		
+		for i in 0..<dataPoints.count {
+			counter += 1.0
+			let dataEntry = BarChartDataEntry(x: values[i], y: counter)
+			dataEntries.append(dataEntry)
+		}
+		
+		let chartDataSet = BarChartDataSet(values: dataEntries, label: "Time")
+		let chartData = BarChartData()
+		chartData.addDataSet(chartDataSet)
+		chartView.data = chartData
+	}
+	
+	
+	
 	
 }
