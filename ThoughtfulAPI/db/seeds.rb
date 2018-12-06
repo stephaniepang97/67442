789b537aa287f08eddfd3a1512774b796b5a978c
stephaniepang97 <stephaniepang97@gmail.com>
2018-12-06 13:47:22 -0500

    Dennis pics

--- ThoughtfulAPI/db/seeds.rb
+++ ThoughtfulAPI/db/seeds.rb
@@ -10,7 +10,7 @@ pang    = Family.create(family_name: 'Pang')
 lam     = Family.create(family_name: 'Lam')
 heimann = Family.create(family_name: 'Heimann')
 
-# lam family
+# heimann family
 User.create(first_name: 'Dennis',
             last_name: 'Smith',
             role: 'patient',
@@ -51,7 +51,7 @@ User.create(first_name: 'John',
             last_name: 'Doe',
             role: 'caretaker',
             family: heimann)
-User.create(first_name: 'Larry',
+larry = User.create(first_name: 'Larry',
             last_name: 'Heimann',
             role: 'caretaker',
             family: heimann)
@@ -59,54 +59,113 @@ User.create(first_name: 'Larry',
 # questions
 Question.create(question: 'Who is this?',
                 answer: 'Alec Lam',
-                created_by: sue.id,
+                created_by: larry.id,
                 attachment: Rails.root.join("db/images/alec.jpg").open)
 Question.create(question: 'Who is this?',
                 answer: 'Frank Liao',
-                created_by: alec.id,
+                created_by: larry.id,
                 attachment: Rails.root.join("db/images/frank.jpg").open)
 Question.create(question: 'Who is this?',
                 answer: 'Gary Dilisio',
-                created_by: alec.id,
+                created_by: larry.id,
                 attachment: Rails.root.join("db/images/gary.jpg").open)
 Question.create(question: 'Who is this?',
                 answer: 'Jordan Stapinski',
-                created_by: alec.id,
+                created_by: larry.id,
                 attachment: Rails.root.join("db/images/jordan.jpg").open)
 Question.create(question: 'Who is this?',
                 answer: 'Stephanie Pang',
-                created_by: alec.id,
+                created_by: larry.id,
                 attachment: Rails.root.join("db/images/steph.jpg").open)
 Question.create(question: 'Who is this?',
                 answer: 'Jose Vazquez',
-                created_by: sue.id,
+                created_by: larry.id,
                 attachment: Rails.root.join("db/images/jose.png").open)
 Question.create(question: 'Who is this?',
                 answer: 'Rudy Wolfs',
-                created_by: sue.id,
+                created_by: larry.id,
                 attachment: Rails.root.join("db/images/rudy.png").open)
 Question.create(question: 'Who is this?',
                 answer: 'Prof H',
-                created_by: sue.id,
+                created_by: larry.id,
                 attachment: Rails.root.join("db/images/profh.jpg").open)
 Question.create(question: 'Who is this?',
                 answer: 'Prof Raja',
-                created_by: sue.id,
+                created_by: larry.id,
                 attachment: Rails.root.join("db/images/raja.jpg").open)
 Question.create(question: 'Where is this?',
                 answer: 'Capital One Bank',
-                created_by: alec.id,
+                created_by: larry.id,
                 attachment: Rails.root.join("db/images/capital-one.jpg").open)
 Question.create(question: 'Where is this?',
                 answer: 'Carnegie Mellon University',
-                created_by: alec.id,
+                created_by: larry.id,
                 attachment: Rails.root.join("db/images/cmu.jpg").open)
 Question.create(question: 'Where is this?',
                 answer: 'Gates Hillman Center',
-                created_by: alec.id,
+                created_by: larry.id,
                 attachment: Rails.root.join("db/images/gates.jpg").open)
 Question.create(question: 'Where is this?',
                 answer: 'Hunt Library',
-                created_by: alec.id,
+                created_by: larry.id,
                 attachment: Rails.root.join("db/images/hunt.jpg").open)
+
+
+# lam family questions
+Question.create(question: 'Who is this?',
+                answer: 'Alec Lam',
+                created_by: sue.id,
+                attachment: Rails.root.join("db/images/dennis/alec.jpg").open)
+Question.create(question: 'Who is this?',
+                answer: 'Dr. Econopouley',
+                created_by: sue.id,
+                attachment: Rails.root.join("db/images/dennis/drEconopouley.jpg").open)
+Question.create(question: 'Who is this?',
+                answer: 'Dr. Joseph',
+                created_by: sue.id,
+                attachment: Rails.root.join("db/images/dennis/drJoseph.jpg").open)
+Question.create(question: 'Who is this?',
+                answer: 'Dr. Stein',
+                created_by: sue.id,
+                attachment: Rails.root.join("db/images/dennis/drStein.jpg").open)
+Question.create(question: 'Who is this?',
+                answer: 'Evan Lam',
+                created_by: sue.id,
+                attachment: Rails.root.join("db/images/dennis/evan.jpg").open)
+Question.create(question: 'Who is this?',
+                answer: 'Gladys',
+                created_by: sue.id,
+                attachment: Rails.root.join("db/images/dennis/gladys.jpg").open)
+Question.create(question: 'Who is this?',
+                answer: 'Jeff',
+                created_by: sue.id,
+                attachment: Rails.root.join("db/images/dennis/jeff.jpg").open)
+Question.create(question: 'Who is this?',
+                answer: 'Joy (nurse)',
+                created_by: sue.id,
+                attachment: Rails.root.join("db/images/dennis/joyNurse.jpg").open)
+Question.create(question: 'Where is this?',
+                answer: 'The pharmacy',
+                created_by: sue.id,
+                attachment: Rails.root.join("db/images/dennis/pharmacy.jpg").open)
+Question.create(question: 'Who is this?',
+                answer: 'Sharon (Neighbor)',
+                created_by: sue.id,
+                attachment: Rails.root.join("db/images/dennis/sharonNeighbor.jpg").open)
+Question.create(question: 'Who is this?',
+                answer: 'Sharton',
+                created_by: sue.id,
+                attachment: Rails.root.join("db/images/dennis/sharton.jpg").open)
+Question.create(question: 'Who is this?',
+                answer: 'Sue Lam',
+                created_by: sue.id,
+                attachment: Rails.root.join("db/images/dennis/sue.jpg").open)
+Question.create(question: 'Where is this?',
+                answer: "Trader Joe's",
+                created_by: sue.id,
+                attachment: Rails.root.join("db/images/dennis/traderJoes.jpg").open)
+Question.create(question: 'What is this?',
+                answer: 'Voting',
+                created_by: sue.id,
+                attachment: Rails.root.join("db/images/dennis/votingMemory.jpg").open)
 
